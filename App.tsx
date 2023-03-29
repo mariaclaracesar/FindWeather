0a2227766a0a43102ba3938758b237d542a8b6e5
Maria Clara <maria.clara@wisereducacao.com>
2023-03-29 00:23:01 -0300

    adicionando styles

--- App.tsx
+++ App.tsx
@@ -1,20 +1,29 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
+import {
+  useFonts,
+  Overpass_300Light,
+  Overpass_400Regular,
+  Overpass_600SemiBold,
+  Overpass_700Bold,
+} from "@expo-google-fonts/overpass";
+import { Home } from './src/screens/Home';
+
+
 export default function App() {
+  const [fontsLoaded] = useFonts({
+    Overpass_300Light,
+    Overpass_400Regular,
+    Overpass_600SemiBold,
+    Overpass_700Bold,
+  });
+
+  if (!fontsLoaded) {
+    return;
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+  <Home />
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ src/screens/Home/styles.ts
@@ -0,0 +1,9 @@
+import styled from 'styled-components'
+
+export const Container = styled.View`
+	width: 100%;
+`;
+
+export const TextView = styled.Text``;
+
+export const TextDescription = styled.Text``;
